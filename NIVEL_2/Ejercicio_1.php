6668749eea808635e91c84a69a3eb5aa0a336226
egido-quan <isidro.zapater@gmail.com>
2024-07-07 02:07:54 +0200

    Modificación ejercicios según comentarios de Rubén

--- NIVEL_2/Ejercicio_1.php
+++ NIVEL_2/Ejercicio_1.php
@@ -9,53 +9,46 @@
 
 <?php
    
-    class PokerDice {
-
-        //Atributos
-        private $num = array(1, 2, 3, 4, 5, 6);
-        private $cara = array("As", "K", "Q", "J", "7", "8");
-        protected  static $tiradas = 0;
-
-        public function throw(): int {
-            self::$tiradas ++;
-            return $this->num[rand(1, 6)-1];            
-        }
-        public function shapeName(int $n): string {
-            return $this->cara[$n - 1];
-            
-        }
-        public static function getTotalThrows(): void {
-            echo "<h3>Dados tirados en total: " . self::$tiradas . "</h3>";
-        }
-    }
+    require "ClaseDado.php";
     
     //Declaramos 5 dados que luego tiraremos
-    $dado1 = new PokerDice();
-    $dado2 = new PokerDice();
-    $dado3 = new PokerDice();
-    $dado4 = new PokerDice();
-    $dado5 = new PokerDice();
+    $cincoDados = array();
+    for ($i = 0; $i < 5; $i++) {
+        $cincoDados[$i] = new PokerDice();
+    } 
 
-    tirarCincoDados($dado1, $dado2, $dado3, $dado4, $dado5);
-    echo "<br>";
-    tirarCincoDados($dado1, $dado2, $dado3, $dado4, $dado5);
-    echo "<br>";
-    tirarCincoDados($dado1, $dado2, $dado3, $dado4, $dado5);
-    echo "<br>";
-    tirarCincoDados($dado1, $dado2, $dado3, $dado4, $dado5);
-    echo "<br>";
 
-    PokerDice::getTotalThrows();
-
-    function tirarCincoDados(PokerDice $dado1, PokerDice $dado2, PokerDice $dado3, PokerDice $dado4, PokerDice $dado5): void {
-        echo "<br>Tiramos 5 dados:<br>";
-        $n1 = $dado1->shapeName($dado1->throw());
-        $n2 = $dado2->shapeName($dado2->throw());
-        $n3 = $dado3->shapeName($dado3->throw());
-        $n4 = $dado4->shapeName($dado4->throw());
-        $n5 = $dado5->shapeName($dado5->throw());
-        echo $n1 . "  " . $n2 . " " . $n3 . " " . $n4 . " " . $n5;      
+    for ($i = 0; $i < rand(4, 10); $i++) { // Haremos entre 4 y 10 tiradas de 5 dados
+        $tiradas = tirarCincoDados($cincoDados);
+        echo "<br>";
+        mostrarCaras($cincoDados, $tiradas);
+        echo "<br>";
     }
+
+
+    PokerDice::getTotalThrows(); //Mostramos el número total de dados tiradas
+
+
+
+    //Tiramos los 5 dados y obtenemos el número de cara
+    function tirarCincoDados(array $cincoDados): array {
+        $tiradas = array();
+        echo "<br>Tiramos 5 dados:<br>";
+        echo "Número de cara: ";
+        for ($i = 0; $i < count($cincoDados); $i++) {
+            $tiradas[] = $cincoDados[$i]->throw();
+            echo $tiradas[$i] . " ";
+        }    
+        return $tiradas;
+    }
+    //Obtenemos la figura de póker asociada a la cara obtenida en cada dado
+    function mostrarCaras(array $cincoDados, array $tiradas): void {
+        echo "Figura de la cara: ";
+        for ($i = 0; $i < count($tiradas); $i++) {
+            echo $cincoDados[$i]->shapeName($tiradas[$i]) . " ";
+        }
+    }
+
 
 ?>   
 
